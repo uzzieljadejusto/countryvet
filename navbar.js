b6074e9ab385edab78eb66ce6ed4182f5003e975
uzzieljadejusto <166095585+uzzieljadejusto@users.noreply.github.com>
2024-04-06 01:48:01 +0800

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,11 @@
+function showNavLinks() {
+  let nav = document.getElementById("navbar-links");
+
+  if (nav.style.display === "none") {
+    nav.style.display = "block";
+  } else {
+    nav.style.display = "none";
+  }
+  if (nav.style.display === "block") {
+  }
+}
